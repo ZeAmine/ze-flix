950cbcffa6b57a6620d53d111efd99a2b3807381
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2022-09-08 15:32:08 +0200

    🌀

--- src/components/Slices/SearchBar/index.jsx
+++ src/components/Slices/SearchBar/index.jsx
@@ -2,9 +2,19 @@ import css from "./style.module.scss";
 import cx from "classnames/bind";
 
 const SearchBar = () => {
-  return <section className={css.SearchBar}>
-    
-  </section>;
+  return (
+    <section className={css.SearchBar}>
+      <div className={css.wrapper}>
+        <div className={css.inputContainer}>
+          <input
+            type="text"
+            className={css.input}
+            placeholder="(Rechercher...)"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SearchBar;
